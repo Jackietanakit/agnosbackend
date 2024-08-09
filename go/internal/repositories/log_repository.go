0209c3880070cfaa020081b741c6acd8412ca3de
Tanakit Thangajantaraprapab <87474610+Jackietanakit@users.noreply.github.com>
2024-08-09 17:16:54 +0700

    database connection env

--- go/internal/repositories/log_repository.go
+++ go/internal/repositories/log_repository.go
@@ -4,12 +4,22 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func StoreLog(request map[string]string, response map[string]interface{}) {
-	db, err := sql.Open("postgres", "user=admin password=qwerqwer dbname=agnos_db sslmode=disable")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
+		dbUser, dbPassword, dbName, dbHost, dbPort)
+
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		fmt.Println(err)
 		return
